14fcbe2da371113727dcf2647707e454943368a8
CHR-onicles <tpandivine48@gmail.com>
2021-09-04 17:43:31 +0000

    building middle nav

--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -154,7 +154,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63139" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52267" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
